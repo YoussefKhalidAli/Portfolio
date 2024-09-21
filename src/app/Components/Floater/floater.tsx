15f3b0d177351b4db2f1f8ccc2ab3e0d202f8542
YoussefKhaildAli <yossofwd@gmail.com>
2024-09-21 19:11:42 +0300

    Final design changes

--- src/app/Components/Floater/floater.tsx
+++ src/app/Components/Floater/floater.tsx
@@ -1,18 +1,23 @@
 // Imported Styles
 import styles from "./floater.module.css";
 
+// Imported Utils
+import Image from "next/image";
+
 // Imported Types
 import { FloaterProps } from "@/app/Types/types";
 
 export default function FloaterComponent({ technology }: FloaterProps) {
   return (
-    <img
+    <Image
       className={styles.tech}
       src={technology.path}
       style={{
         top: `${technology.positionY}vh`,
         left: `${technology.positionX}vw`,
       }}
+      width={40}
+      height={40}
       alt="Technology"
     />
   );
--- src/app/Components/UI/Modal/modal.tsx
+++ src/app/Components/UI/Modal/modal.tsx
@@ -1,6 +1,9 @@
 // Imported Styles
 import styles from "./Modal.module.css";
 
+// Imported Utils
+import Image from "next/image";
+
 // Imported Types
 import { ModalProps } from "@/app/Types/types";
 
@@ -8,7 +11,13 @@ export default function ModalComponent({ settings }: ModalProps) {
   return (
     <div className={styles.overlay} onClick={() => settings.closeModal()}>
       <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-        <img src={settings.path} className={styles.image} />
+        <Image
+          src={settings.path}
+          className={styles.image}
+          alt="Certificate"
+          layout="fill"
+          objectFit="contain"
+        />
       </div>
     </div>
   );
