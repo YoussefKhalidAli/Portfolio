e12e596c749d8ad8554e0cbf81fe74e6ae25e46b
YoussefKhaildAli <yossofwd@gmail.com>
2024-09-25 09:19:47 +0300

    Added Mixpanel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 // Imported Components
 import HomePage from "./pages/Home/page";
 
 //Import Utils
+import { useEffect } from "react";
 import mixpanel from "mixpanel-browser";
 
 export default function Home() {
-  mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_PROJECT_TOKEN!, {
-    debug: true,
-  });
+  useEffect(() => {
+    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_PROJECT_TOKEN!, {
+      debug: true,
+    });
+  }, []);
 
   return <HomePage></HomePage>;
 }
