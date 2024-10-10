c69b394dd72886527a3aaafee5d9086d15c73982
YoussefKhaildAli <yossofwd@gmail.com>
2024-10-10 20:42:29 +0300

    Changed Professional Summary

--- src/app/Components/Hero/hero.tsx
+++ src/app/Components/Hero/hero.tsx
@@ -10,15 +10,12 @@ export default function HeroComponent() {
       <div className={styles.heroContent}>
         <h1>Youssef Khalid Abdullah Ali</h1>
         <p>
-          Driven and detail-oriented Computer Science student with hands-on
-          experience in full-stack web development. Proficient in both front-end
-          and back-end technologies, including React.js, Node.js, and MongoDB.
+          Software developer with hands-on experience in full-stack web
+          development. Proficient in both front-end and back-end technologies.
           Adept at creating scalable, user-centric web applications and mobile
           solutions. Demonstrated expertise in software development lifecycle
-          management, problem-solving, and collaborative teamwork. Passionate
-          about leveraging my technical skills to contribute to innovative
-          projects and drive technological advancements. Eager to take on new
-          challenges in dynamic development environments.
+          management, problem-solving, and collaborative teamwork. Eager to take
+          on new challenges in dynamic development environments.
         </p>
         <div className={styles.ctaButtons}>
           <a href="#projects" className={styles.btnPrimary}>
